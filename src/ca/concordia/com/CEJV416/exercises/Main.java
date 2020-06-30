4cb1fc1b23c07d7ecbd5e57efd2edd59a446d7b8
Rizwan Aslam <rizwan.aslam0@gmail.com>
2020-06-29 21:28:40 -0400

    finish class 2

---
+++ src/ca/concordia/com/CEJV416/exercises/Main.java
@@ -0,0 +1,21 @@
+package ca.concordia.com.CEJV416.exercises;
+
+public class Main {
+
+    public static void main(String[] args){
+        int num1= 5;
+        int num2= 6;
+        int num3= 7;
+        int num4= 9;
+
+        System.out.println("hello world"+(num1+num2));
+        System.out.println("calculate"+(num4+num1));
+        System.out.println("course is good");
+        System.out.println(num2+num3);
+        System.out.println("hello"+(num1+num3));
+        System.out.println("world"+(num1+num2));
+
+
+
+   }
+}
---
+++ src/ca/concordia/com/CEJV416/exercises/class_2.java
@@ -0,0 +1,15 @@
+package ca.concordia.com.CEJV416.exercises;
+
+public class class_2 {
+
+    public static void main(String[] args){
+        int num1 = 2;
+        int num = 4;
+
+        // System.out
+
+
+
+
+    }
+}
